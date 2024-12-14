3556ed9f3f225ed914cf1c97cbaa90c94ad13a93
ZR Zheng <zzr.amber@gmail.com>
2024-12-14 15:10:07 +0800

    添加live2d，换成血小板模型

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
-// const live2d_path = "https://cdn.jsdelivr.net/gh/ZR-Zheng/live2d-widget@latest/";
-const live2d_path = "";
+const live2d_path = "https://cdn.jsdelivr.net/gh/ZR-Zheng/bloghelper@latest/live2d/";
+// const live2d_path = "";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/ZR-Zheng/bloghelper@latest/live2d/api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
